e6ce4396c9df0fd376ba7fcc47d124670eaec81c
Abdul Ahad Butt <abdul-ahad-butt@users.noreply.github.com>
2026-08-30 02:52:52 -0700

    Update scraper.py: fix referer URL in headers

--- scraper.py
+++ scraper.py
@@ -84,7 +84,7 @@ def scrape_adbuq():
     
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Referer": "https://www.google.com/",
+        "Referer": "https://www.adbuq.com/",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5"
     }
